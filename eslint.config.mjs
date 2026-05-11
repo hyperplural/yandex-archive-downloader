4900bc69fada4d385a425fe6f1715dc5851d09ab
hyperplural <thundercloudme@gmail.com>
2026-05-11 20:59:16 +0300

    docs: add screenshot to README and ignore release artifacts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**']
+    ignores: ['dist/**', 'release/**', 'node_modules/**']
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
